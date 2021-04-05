8849868116d9d89e91d6818ff94c7341b226b43e
Jahongir Sobirov <69193276+Jahongir2007@users.noreply.github.com>
2021-04-05 21:23:27 +0500

    Create calcpy.py

---
+++ bin/calcpy.py
@@ -0,0 +1,18 @@
+'''
+    Author: Jahongir Sobirov
+    Calc.py python library
+    Version: 1.0.0
+    License: MIT
+'''
+def add(a, b):
+    c = a + b
+    print(c)
+def multipi(a, b):
+    c = a - b
+    print(c)
+def div(a, b):
+    c = a / b
+    print(c)
+def multipl(a, b):
+    c = a * b
+    print(c)
